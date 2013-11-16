2820b0f7aadd6eeb21ecf36669c56c6b368f1f6a
aryann <aryannm@gmail.com>
2013-11-16 22:01:45 +0100

    Removed tests

--- JavaUtils/src/collections/CircularArrayList.java
+++ JavaUtils/src/collections/CircularArrayList.java
@@ -1,7 +1,6 @@
 package collections;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 @SuppressWarnings("serial")
 public class CircularArrayList<E> extends ArrayList<E> {
@@ -39,48 +38,5 @@ public class CircularArrayList<E> extends ArrayList<E> {
 			removeElement(last);
 		}
 		
-	}
-
-	public static void main(String[] args) {
-		long t0 = System.nanoTime();
-		for (int i = 0; i < 1E+8; i++) {
-			CircularArrayList<String> list = new CircularArrayList<>();
-			list.add("This");
-			list.add("Is");
-			list.add("A");
-			list.add("Very");
-			list.add("Nice");
-			list.add("And");
-			list.add("Cool");
-			list.add("Sentence");
-
-			for (Iterator<String> iter = list.iterator(); iter.hasNext();) {
-				iter.next();
-				iter.remove();
-			}
-		}
-		double t1 = System.nanoTime();
-		System.out.println("Time spent circular: " + (1.0 * t1 - t0) / (1E+9));
-
-		t0 = System.nanoTime();
-		for (int i = 0; i < 1E+8; i++) {
-			List<String> list = new ArrayList<>();
-			list.add("This");
-			list.add("Is");
-			list.add("A");
-			list.add("Very");
-			list.add("Nice");
-			list.add("And");
-			list.add("Cool");
-			list.add("Sentence");
-
-			for (Iterator<String> iter = list.iterator(); iter.hasNext();) {
-				iter.next();
-				iter.remove();
-			}
-		}
-		t1 = System.nanoTime();
-		System.out.println("Time spent normal: " + (1.0 * t1 - t0) / (1E+9));
-
 	}
 }
